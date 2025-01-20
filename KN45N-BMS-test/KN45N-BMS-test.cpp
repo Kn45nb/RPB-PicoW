4cc0dc1568dd982495110eeb51543e19a73f6828
Kiên <110784389+Kn45nb@users.noreply.github.com>
2025-01-20 15:08:57 +0700

    Update KN45N-BMS-test.cpp

--- KN45N-BMS-test/KN45N-BMS-test.cpp
+++ KN45N-BMS-test/KN45N-BMS-test.cpp
@@ -27,6 +27,10 @@ Defines     Var                             Val             Mô tả
 #define     I2C_PORT                        i2c0
 #define     I2C_SDA                         8
 #define     I2C_SCL                         9
+// SSD1306 defines
+#define     SSD1306_I2C_ADDR                0x3C            // Địa chỉ I2C của SSD1306
+#define     SCREEN_WIDTH                    128
+#define     SCREEN_HEIGHT                   64
 
 // UART defines (default UART 'uart0')
 #define     UART_ID                         uart1
@@ -88,6 +92,79 @@ const   char        serialNumber[]          = "0001";       // Số serial duy n
 
 
 /*============================================================================================================================================================================
+Sub Funsion
+============================================================================================================================================================================*/
+// Lệnh khởi tạo SSD1306
+const uint8_t ssd1306_init_cmds[] = 
+{
+    0xAE,       // Display OFF
+    0xD5, 0x80, // Set Display Clock Div
+    0xA8, 0x3F, // Set Multiplex Ratio (1/64)
+    0xD3, 0x00, // Set Display Offset
+    0x40,       // Set Start Line Address
+    0x8D, 0x14, // Enable charge pump
+    0x20, 0x00, // Set Memory Addressing Mode (Horizontal addressing mode)
+    0xA1,       // Set Segment Re-map (mirror horizontally)
+    0xC8,       // Set COM Output Scan Direction (mirror vertically)
+    0xDA, 0x12, // Set COM Pins Hardware Configuration
+    0x81, 0xCF, // Set Contrast Control
+    0xD9, 0xF1, // Set Pre-charge Period
+    0xDB, 0x40, // Set VCOMH Deselect Level
+    0xA4,       // Disable Entire Display On
+    0xA6,       // Set Normal Display
+    0xAF        // Display ON
+};
+
+// Hàm gửi lệnh tới SSD1306
+void ssd1306_send_command(uint8_t cmd)
+{
+    uint8_t buffer[2] = {0x00, cmd};
+    i2c_write_blocking(I2C_PORT, SSD1306_I2C_ADDR, buffer, 2, false);
+}
+
+// Hàm gửi dữ liệu tới SSD1306
+void ssd1306_send_data(uint8_t *data, size_t len)
+{
+    uint8_t buffer[len + 1];
+    buffer[0] = 0x40; // Byte đầu tiên là chỉ báo dữ liệu
+    memcpy(&buffer[1], data, len);
+    i2c_write_blocking(I2C_PORT, SSD1306_I2C_ADDR, buffer, len + 1, false);
+}
+
+// Hàm khởi tạo SSD1306
+void ssd1306_init()
+{
+    for (size_t i = 0; i < sizeof(ssd1306_init_cmds); i++) {
+        ssd1306_send_command(ssd1306_init_cmds[i]);
+    }
+}
+
+// Hàm làm sạch màn hình SSD1306
+void ssd1306_clear()
+{
+    uint8_t buffer[SCREEN_WIDTH * SCREEN_HEIGHT / 8] = {0};
+    for (uint8_t page = 0; page < 8; page++) {
+        ssd1306_send_command(0xB0 + page); // Set Page Start Address
+        ssd1306_send_command(0x00);       // Set Lower Column Start Address
+        ssd1306_send_command(0x10);       // Set Higher Column Start Address
+        ssd1306_send_data(buffer, SCREEN_WIDTH);
+    }
+}
+
+// Hàm hiển thị chuỗi ký tự trên SSD1306
+void ssd1306_display_text(const char *text)
+{
+    // Ví dụ hiển thị dữ liệu cứng nhắc. Muốn mềm dẻo hơn cần thư viện GFX.
+    uint8_t text_data[SCREEN_WIDTH] = {0xFF, 0x81, 0x81, 0x81, 0xFF}; // Khung ký tự "A".
+    ssd1306_send_command(0xB0);  // Page Start Address
+    ssd1306_send_command(0x00);  // Lower Column Start Address
+    ssd1306_send_command(0x10);  // Higher Column Start Address
+    ssd1306_send_data(text_data, sizeof(text_data));
+}
+
+
+
+/*============================================================================================================================================================================
 Main Funsion
 ============================================================================================================================================================================*/
 int main()
@@ -102,16 +179,18 @@ int main()
     }
 
     // SPI initialisation
-    spi_init(SPI_PORT, 1000*1000);                  // 1MHz
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
+    // spi_init(SPI_PORT, 1000*1000);                  // 1MHz
+    // gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    // gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
+    // gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+    // gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    // // Chip select is active-low, so we'll initialise it to a driven-high state
+    // gpio_set_dir(PIN_CS, GPIO_OUT);
+    // gpio_put(PIN_CS, 1);
     // https://github.com/raspberrypi/pico-examples/tree/master/spi
+
+
+
 
     // I2C Initialisation
     i2c_init(I2C_PORT, 400*1000);                   // 400Khz
@@ -121,36 +200,53 @@ int main()
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
     // https://github.com/raspberrypi/pico-examples/tree/master/i2c
+    // Khởi tạo SSD1306
+    ssd1306_init();
+    ssd1306_clear();
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+    // Hiển thị "Hello"
+    ssd1306_display_text("Hello");
 
     while (true)
     {
-
-        printf("Hello, world!\n");
+        tight_loop_contents();
+        // Example to turn on the Pico W LED
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         
-        sleep_ms(1000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     }
+
+    return 0;
+    
+    
+    
+    
+    
+    
+
+    // // Set up our UART
+    // uart_init(UART_ID, BAUD_RATE);
+    // // Set the TX and RX pins by using the function select on the GPIO
+    // // Set datasheet for more information on function select
+    // gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    // gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+
+
+
+
+    // Send out a string, with CR/LF conversions
+    // uart_puts(UART_ID, " Hello, UART!\n");
+    
+    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+    // while (true)
+    // {
+
+    //     printf("Hello, world!\n");
+        
+    //     sleep_ms(1000);
+    // }
 }
-
-/*============================================================================================================================================================================
-Sub Funsion
-============================================================================================================================================================================*/
-
-
