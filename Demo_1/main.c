2cd4eb717832e4eb4846ae6ab12169c45b1ebc6f
Kiên <knight.45.acgg@gmail.com>
2025-09-02 17:29:11 +0700

    Refactor and document main.c with detailed variable setup
    Added extensive documentation headers, organized and expanded variable definitions for battery management, and restructured code sections for clarity. This improves maintainability and provides clear context for hardware configuration and battery-related data structures.

--- Demo_1/main.c
+++ Demo_1/main.c
@@ -1,29 +1,104 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/spi.h"
-#include "hardware/i2c.h"
-#include "hardware/pio.h"
-#include "hardware/uart.h"
+/*=============================================================
+Information
+===============================================================
+SDK: 		v2.2.0
+Toolchain:	13.3Rel1
+Ninja:		v1.12.1
+CMake:		v3.31.4
+Board:		pico_w
+Project:    Demo_1
+Author:		@Kn45nb
+Date:		
+License:	MIT License
+=============================================================*/
 
-// SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
 
-#include "blink.pio.h"
+/*=============================================================
+Include	    Lib
+=============================================================*/
+#include    <stdio.h>
+#include    "hardware/uart.h"
+#include    "pico/stdlib.h"
+#include    "hardware/spi.h"
+#include    "hardware/i2c.h"
+#include    "hardware/pio.h"
+#include    "hardware/uart.h"
+#include    "blink.pio.h"
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+
+
+/*=============================================================
+Define  Variables   Values
+=============================================================*/
+#define SPI_PORT    spi0
+#define PIN_MISO    16
+#define PIN_CS      17
+#define PIN_SCK     18
+#define PIN_MOSI    19
+
+#define I2C_PORT    i2c0
+#define I2C_SDA     8
+#define I2C_SCL     9
+
+#define UART_ID uart1
+#define BAUD_RATE 115200
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+
+
+/*=============================================================
+Const/Var   Type        Variables                   Values                  Description                                         Unit            Note
+=============================================================*/
+// Commant of Master
+            uint32_t    CMD                         = 0x0;                  // Command of Master                                // N/a          //
+// Package: _BIX
+const       uint32_t    REVISION                    = 0x0;                  // Version of the data structure _BIX               // N/a          // Basic: 0x0 (?const)
+const       uint32_t    POWER_UNIT                  = 0x0;                  // Biến giá trị đơn vị                              // N/a          // 0: mWh, 1: mAh
+const       uint32_t    DESIGN_CAPACITY             = 0x186A0;              // Dung lượng thiết kế của pin                      // POWER_UNIT   // !set: 0x0 || 0xFFFFFFFF
+            uint32_t    LAST_FULL_CHARGE_CAPACITY   = 0x186A0;              // Dung lượng sạc đầy cuối cùng của pin             // POWER_UNIT   // !set: 0x0 || 0xFFFFFFFF, max: DESIGN_CAPACITY, @Kn45nb Cần một bộ logic i++, khởi tạo là DESIGN_CAPACITY trừ dần mỗi lần time full.
+const       uint32_t    BATTERY_TECHNOLOGY          = 0x1;                  // Công nghệ tái tạo pin (khả năng sạc)             // N/a          // 0: No, 1: Yes
+const       uint32_t    DESIGN_VOLTAGE              = 0x4A38;               // Điện áp thiết kế của pin                         // mV           // !set: 0x0 || 0xFFFFFFFF, Basic voltage: 19V/12V
+            uint32_t    DESIGN_CAPACITY_OF_WARNING  = 0x2710;               // Dung lượng cảnh báo thiết kế của pin             // POWER_UNIT   // Windows ignores this value (Nhưng Bios thì rất hay đọc)
+            uint32_t    DESIGN_CAPACITY_OF_LOW      = 0x1388;               // Dung lượng thấp thiết kế của pin (hibernation)   // POWER_UNIT   // 0-5% of LAST_FULL_CHARGE_CAPACITY, @Kn45nb Cần fun hoặc logic cập nhật sau khi update LAST_FULL_CHARGE_CAPACITY
+const       uint32_t    CAPACITY_GRANULARITY_1      = 0x1;                  // Độ tinh mịch (phân giải) dung lượng trên ngưỡng  // POWER_UNIT   // <=1% of DESIGN_CAPACITY
+const       uint32_t    CAPACITY_GRANULARITY_2      = 0x1;                  // Độ tinh mịch (phân giải) dung lượng dưới ngưỡng  // POWER_UNIT   // <=75mW, (exp: 0.25% of 25000mWh)
+            uint32_t    CYCLE_COUNT                 = 0x0;                  // Số lần sạc của pin                               // Times        // !set: 0xFFFFFFFF
+const       uint32_t    MEASUREMENT_ACCURACY        = 0x1;                  // Độ chính xác đo lường                            // PPM          // >95000 (95%), 1%=1000
+const       uint32_t    MAX_SAMPLING_TIME           = 0x1;                  // Thời gian lấy mẫu tối đa của _BST                // ms           //
+const       uint32_t    MIN_SAMPLING_TIME           = 0x1;                  // Thời gian lấy mẫu tối thiểu của _BST             // ms           //
+const       uint32_t    MAX_AVERAGING_INTERVAL      = 0x1;                  // Thời gian trung bình max của cảm biến PIN        // ms           //
+const       uint32_t    MIN_AVERAGING_INTERVAL      = 0x1;                  // Thời gian trung bình min của cảm biến PIN        // ms           //
+const       char        MODEL_NUMBER[]              = "Notebook";           // Mã số model của pin                              // N/a          // !NULL
+const       char        SERIAL_NUMBER[]             = "0001";               // Số serial duy nhất của pin                       // N/a          // !NULL
+const       char        BATTEY_TYPE[]               = "LION";               // Loại pin                                         // N/a          //
+const       char        OEM_INFORMATION[]           = "MAKE BY @Kn45nb";    // Thông tin OEM                                    // N/a          //
+
+
+// Package: _BST
+            uint32_t    BATTERY_STATE               = 0x1;                  // Trạng thái pin                                   // N/a          // 0x0000: Charging, 0x0001: Discharging, ...
+            uint32_t    BATTERY_PRESENT_RATE        = 0x1;                  // Tốc độ Sạc/xả (Điện áp giữa 2 đầu nguồn điện)    // POWER_UNIT   // !set: 0x0 || 0xFFFFFFFF, (Âm sạc, dương xả) @Kn45nb $Do that
+            uint32_t    BATTEY_REMAINING_CAPACITY   = 0x1;                  // Dung lượng còn lại của pin                       // POWER_UNIT   // !set: 0x0 || 0xFFFFFFFF
+            uint32_t    BATTERY_VOLTAGE             = 0x4A38;               // Điện áp hiện tại của pin                         // mV           // 
+
+// Package: _BIF (old version for Legacy. Don't carefull that 🥲)
+
+
+// Package: _BTP (Checkpoint charging using cơ chế Điểm ngắt pin. Cần dùng trong powercfg/batteryreport @Kn45nb)
+
+
+// Package: _SUN (Option: Số đơn vị khe cắm, nếu chơi thứ tự pin thì không cần @Kn45nb)
+
+
+// Package: _STA (Sau làm Funsion Shutdows PSU/PIN thì mới động tới @Kn45nb)
+
+
+/*=============================================================
+Method
+=============================================================*/
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq)
+{
     blink_program_init(pio, sm, offset, pin);
     pio_sm_set_enabled(pio, sm, true);
 
@@ -34,17 +109,9 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
     pio->txf[sm] = (125000000 / (2 * freq)) - 3;
 }
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
-
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
-
-
+/*=============================================================
+Main
+=============================================================*/
 
 int main()
 {
